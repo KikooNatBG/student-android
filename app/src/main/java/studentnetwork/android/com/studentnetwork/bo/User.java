4728f7be6e9dc846e4e110342aba420a604501f0
Antony Vitré <antony@eliga.fr>
2018-04-23 14:09:41 +0200

    Ajout Register Service + Vue

---
+++ app/src/main/java/studentnetwork/android/com/studentnetwork/bo/User.java
@@ -0,0 +1,123 @@
+package studentnetwork.android.com.studentnetwork.bo;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String aboutMe;
+    private Date registerDate;
+    private Date birthDate;
+    private String pictureUrl;
+    private String smallPictureUrl;
+    private String largePictureUrl;
+    private String type;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getSmallPictureUrl() {
+        return smallPictureUrl;
+    }
+
+    public void setSmallPictureUrl(String smallPictureUrl) {
+        this.smallPictureUrl = smallPictureUrl;
+    }
+
+    public String getLargePictureUrl() {
+        return largePictureUrl;
+    }
+
+    public void setLargePictureUrl(String largePictureUrl) {
+        this.largePictureUrl = largePictureUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
