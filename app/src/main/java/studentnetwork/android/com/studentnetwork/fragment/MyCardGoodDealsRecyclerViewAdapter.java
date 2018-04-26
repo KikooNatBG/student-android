6889270689eb3324cb91023fce61e002e6064982
Unknown <thibaud.moulin.fr@gmail.com>
2018-04-26 13:58:03 +0200

    Fix erreur build :s sorry

--- app/src/main/java/studentnetwork/android/com/studentnetwork/fragment/MyCardGoodDealsRecyclerViewAdapter.java
+++ app/src/main/java/studentnetwork/android/com/studentnetwork/fragment/MyCardGoodDealsRecyclerViewAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import studentnetwork.android.com.studentnetwork.R;
 import studentnetwork.android.com.studentnetwork.fragment.CardGoodDealsFragment.OnListFragmentInteractionListener;
 import studentnetwork.android.com.studentnetwork.fragment.dummy.DummyContent.DummyItem;
 
